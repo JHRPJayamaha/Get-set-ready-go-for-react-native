e2784b11b28f32be80a0dd22870f1682235862af
JHRPjayamaha <rpjayamaha@gmail.com>
2020-05-29 21:47:10 +0530

    change number of lines

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>
+      <Text numberOfLines={1}>
         Hello React Native. this is my first application and this text is realy
         realy long. what happend is long text in react native
       </Text>
@@ -16,5 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
