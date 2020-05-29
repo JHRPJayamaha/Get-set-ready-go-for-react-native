0514009f0ea6efee6b0ab9485b1203fcd7ed306d
JHRPjayamaha <rpjayamaha@gmail.com>
2020-05-29 21:52:44 +0530

    text perssed

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1}>
+      <Text numberOfLines={1} onPress={handlePress}>
         Hello React Native. this is my first application and this text is realy
         realy long. what happend is long text in react native
       </Text>
