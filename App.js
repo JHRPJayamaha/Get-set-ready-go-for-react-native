051104505c7383b32a99e8ca9594ce46ae7ed3e3
JHRPjayamaha <rpjayamaha@gmail.com>
2020-05-30 21:08:13 +0530

    platform specific code

--- App.js
+++ App.js
@@ -5,32 +5,24 @@ import {
   View,
   TouchableNativeFeedback,
   Image,
-  Alert,
   Button,
   SafeAreaView,
+  Platform,
+  StatusBar,
 } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("My Title", "My Message", [
-            { text: "Yes" },
-            { text: "No" },
-          ])
-        }
-      />
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button title="Click Me" onPress={() => console.log("Button Tapped")} />
     </SafeAreaView>
   );
 }
-
+const containerStyle = { backgroundColor: "orange" };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
